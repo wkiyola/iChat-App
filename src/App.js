8426ec4f1ac2d8c47c046060ab1eada8e8c24b08
unknown <wkiyola@yahoo.com>
2021-04-12 18:03:16 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+export default function App() {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID = {process.env.projectID}
+            userName="wkiyola"
+            userSecret="4006"
+            renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps} />}
+        />
+     );
+};
---
+++ src/components/MyMessage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MyMessage({ message }) {
+    return (
+        <div>
+            my message
+        </div>
+    )
+}
